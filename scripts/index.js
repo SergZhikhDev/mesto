2b8e82592114a4a8fe2b5ceb038735785e9fad9b
Sergey Zhikharev <as1234531@yandex.ru>
2022-01-27 00:18:13 +0400

    27.01.2022

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,55 @@
 const profileOpenPopupButton = document.querySelector(".edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = document.querySelector(".popup__close");
-const popupContainer = document.querySelector(".popup__container");
-const submitBtn = document.querySelector(".form__handlers");
+//const popupContainer = document.querySelector(".popup__container");
+//const submitBtn = document.querySelector(".form__handlers");
+let formElement = document.querySelector(".form");
+let nameInput = formElement.querySelector("#name");
+let jobInput = formElement.querySelector("#about-self");
+
+let profInfoName = document.querySelector(".profile-info__name");
+let profInfoAboutSelf = document.querySelector(".profile-info__about-self");
 
 
 function openPopup() {
   popup.classList.add("popup_opened");
 }
-profileOpenPopupButton.addEventListener("click", openPopup);
+
 
 function closePopup() {
   popup.classList.remove("popup_opened");
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  closePopup();
+  profInfoName.textContent = nameInput.value;
+  profInfoAboutSelf.textContent = jobInput.value;
+}
+
+profileOpenPopupButton.addEventListener("click", openPopup);
+
 popupCloseButton.addEventListener("click", closePopup);
 
-submitBtn.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
 
-popupContainer.addEventListener("Enter", closePopup);
+//submitBtn.addEventListener("click", closePopup);
+
+//popupContainer.addEventListener("Enter", closePopup);
+
+
+/*let formElement = document.querySelector(".form");
+let nameInput = formElement.querySelector("#name");
+let jobInput = formElement.querySelector("#about-self");
+
+let profInfoName = document.querySelector(".profile-info__name");
+let profInfoAboutSelf = document.querySelector(".profile-info__about-self");*/
+
+/*function formSubmitHandler(evt) {
+  evt.preventDefault();
+  closePopup();
+  profInfoName.textContent = nameInput.value;
+  profInfoAboutSelf.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", formSubmitHandler);*/
