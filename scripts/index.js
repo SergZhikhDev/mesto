f3b754e9aa6479603f568c69e400c50d22e963b5
Sergey Zhikharev <as1234531@yandex.ru>
2022-01-28 01:23:08 +0400

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
-const profileOpenPopupButton = document.querySelector(".edit-button");
+const profileOpenPopupButton = document.querySelector(".profile-info__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = document.querySelector(".popup__close");
-//const popupContainer = document.querySelector(".popup__container");
-//const submitBtn = document.querySelector(".form__handlers");
 let formElement = document.querySelector(".form");
 let nameInput = formElement.querySelector("#name");
 let jobInput = formElement.querySelector("#about-self");
-
 let profInfoName = document.querySelector(".profile-info__name");
 let profInfoAboutSelf = document.querySelector(".profile-info__about-self");
 
 
 function openPopup() {
   popup.classList.add("popup_opened");
+  nameInput.value = profInfoName.textContent;
+  jobInput.value = profInfoAboutSelf.textContent;
 }
 
 
@@ -22,9 +21,9 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  closePopup();
   profInfoName.textContent = nameInput.value;
   profInfoAboutSelf.textContent = jobInput.value;
+  closePopup();
 }
 
 profileOpenPopupButton.addEventListener("click", openPopup);
@@ -33,23 +32,3 @@ popupCloseButton.addEventListener("click", closePopup);
 
 formElement.addEventListener("submit", formSubmitHandler);
 
-//submitBtn.addEventListener("click", closePopup);
-
-//popupContainer.addEventListener("Enter", closePopup);
-
-
-/*let formElement = document.querySelector(".form");
-let nameInput = formElement.querySelector("#name");
-let jobInput = formElement.querySelector("#about-self");
-
-let profInfoName = document.querySelector(".profile-info__name");
-let profInfoAboutSelf = document.querySelector(".profile-info__about-self");*/
-
-/*function formSubmitHandler(evt) {
-  evt.preventDefault();
-  closePopup();
-  profInfoName.textContent = nameInput.value;
-  profInfoAboutSelf.textContent = jobInput.value;
-}
-
-formElement.addEventListener("submit", formSubmitHandler);*/
