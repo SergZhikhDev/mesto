cb1e84b1fb180a261cfc30109efb3d4b2b0aa5fc
Sergey Zhikharev <as1234531@yandex.ru>
2022-02-19 01:34:52 +0400

    Update exercise.js

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -1,3 +1,5 @@
+//***********************************РАБОТАЕТ*******************************************************************//
+
 const initialCards = [
   // значения для карточек "из коробки"
   {
@@ -26,6 +28,7 @@ const initialCards = [
   },
 ];
 
+<<<<<<< Updated upstream
 const listCards = document.querySelector(".elements"); // список карточек изнаяально пустой, наполняется изначально из массива
 const templateItem = document.querySelector(".item_template").content; // шаблон html разметка карточки
 const cardImage = document.querySelectorAll(".element__image"); //изображение карточки
@@ -53,6 +56,41 @@ const popupAddBlockCloseButton = document.querySelector(
   ".popup__close_add-block"
 ); //кнопка закрытия формы добавить карточку
 const popupPosterCloseButton = document.querySelector(".popup__close_poster"); // кнопка закрытия попапа
+=======
+
+const listCards = document.querySelector(".elements"); // список карточек изнаяально пустой, наполняется изначально из массива
+
+const templateItem = document.querySelector(".item_template").content; // шаблон html разметка карточки
+
+const cardImage = document.querySelectorAll(".element__image");//изображение карточки
+const posterImage = document.querySelector(".poster__image");
+const posterTitle = document.querySelector(".poster__name");
+
+const placeNameInput = document.querySelector("#place");//первая полоса ввода формы Новое место(Add)
+const placeLinkInput = document.querySelector("#placeLink");//вторая полоса ввода формы Новое место(Add)
+
+const nameInput = document.querySelector(".form__input-area_fst");//первая полоса ввода форм(Edit, Add)
+const jobInput = document.querySelector(".form__input-area_snd");//вторая полоса ввода форм(Edit, Add)
+
+const profInfoName = document.querySelector(".profile-info__name");//
+const profInfoAboutSelf = document.querySelector(".profile-info__about-self");//
+
+const profilePopup = document.querySelector(".profilePopup");// попап блока редактировать профиль
+const cardPopup = document.querySelector(".cardPopup");//попап блока добавить карточку
+const posterPopup = document.querySelector(".posterPopup");//попап постера (большой фотографии)
+
+const saveEditPopupBtn = document.querySelector(".form__button_edit-block");//кнопка сохранения формы редактировать профиль
+const saveAddPopupBtn = document.querySelector(".form__button_add-block");//кнопка сохранения формы добавить карточку
+
+const openPopupEditButton = document.querySelector(".profile-info__edit-button");//кнопка открытия формы редактировать профиль
+const openPopupAddButton = document.querySelector(".profile__add-button");//кнопка открытия формы добавить карточку
+
+const popupEditBlockCloseButton = document.querySelector(".popup__close_edit-block");//кнопка закрытия формы редактировать профиль
+const popupAddBlockCloseButton = document.querySelector(".popup__close_add-block");//кнопка закрытия формы добавить карточку
+const popupPosterCloseButton = document.querySelector(".popup__close_poster");// кнопка закрытия попапа
+
+
+>>>>>>> Stashed changes
 
 //функция перебирает значения карточек "из коробки"
 function render() {
@@ -67,6 +105,7 @@ function renderItem(item) {
   cardElement.querySelector(".element__image").setAttribute("src", item.link); //присвоение атрибута
   cardElement.querySelector(".element__image").setAttribute("alt", "на фотографии " + item.name); // склеивает фразу "на фотографии + имя карточки"
   cardElement.querySelector(".element__title").innerText = item.name; // замена текста
+<<<<<<< Updated upstream
   addListeners(cardElement); // добавляет слушателя новому элементу
   addCardAppend(cardElement); // добавляет карточку в конец списка
 }
@@ -106,6 +145,27 @@ function handleProfileFormSubmit(event) {
   profInfoName.textContent = nameInput.value;
   profInfoAboutSelf.textContent = jobInput.value;
   closePopup(profilePopup);
+=======
+addListeners(cardElement); // добавляет слушателя новому элементу
+addCardAppend(cardElement); // добавляет карточку в конец списка
+}
+
+
+
+function createCard(event) {
+  const cardElement = templateItem.cloneNode(true);
+  event.preventDefault();
+
+  cardElement.querySelector(".element__image").setAttribute("src", placeLinkInput.value); //присвоение атрибута
+    cardElement.querySelector(".element__image").setAttribute("alt", "на фотографии " + placeLinkInput.alt); // склеивает фразу "на фотографии + имя карточки"
+    cardElement.querySelector(".element__title").textContent = placeNameInput.value; // замена текста
+
+    addListeners(cardElement);
+  closePopup(cardPopup);
+  addCardPrepend(cardElement)
+  return cardElement;
+
+>>>>>>> Stashed changes
 }
 
 function openPopup(popup) {
@@ -113,6 +173,7 @@ function openPopup(popup) {
   reset();
 }
 
+<<<<<<< Updated upstream
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
@@ -158,6 +219,78 @@ function addListeners(el) {
   el.querySelector(".openPopupBtn__poster").addEventListener("click", () =>
     openImage(event)
   );
+=======
+
+ //Готовые карточки добавляем в конец DOM
+function addCardAppend(cardElement) {
+  listCards.append(cardElement);
+}
+
+
+//Готовые карточки добавляем в начало DOM
+function addCardPrepend(cardElement) {
+  listCards.prepend(cardElement);
+}
+
+
+// функция сохраняет данные первой формы
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profInfoName.textContent =nameInput.value;
+  profInfoAboutSelf.textContent = jobInput.value;
+  closePopup(profilePopup);
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  reset();
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+
+}
+
+//Постер попап
+function openImage(event) {
+  posterImage.src = event.target.src
+  posterImage.alt = event.target.alt
+posterTitle.innerText = event.target.alt
+
+  openPopup(posterPopup)
+}
+
+// Сброс предидущих значение полей ввода форм при новом открытии
+  function reset() {
+    const val = document.querySelectorAll(".form__input-area")
+    val.forEach(resetData);
+  }
+
+ function resetData(item){
+    item.value = '';
+  }
+
+
+ // слушатель запускает ф-ию открыть первую формы по клику на edit-buton на add-buton
+ openPopupEditButton.addEventListener("click",  () =>openPopup(profilePopup));
+ openPopupAddButton.addEventListener("click",  () =>openPopup(cardPopup));
+
+ //слушатель запускает ф-ию закрыть  форму по клику на close-buton
+popupEditBlockCloseButton.addEventListener("click", () =>closePopup(profilePopup));
+popupAddBlockCloseButton.addEventListener("click", () =>closePopup(cardPopup));
+popupPosterCloseButton.addEventListener("click", () =>closePopup(posterPopup));
+
+//слушатель на кнопку "submit"
+saveEditPopupBtn.addEventListener("click", handleProfileFormSubmit);// ЕСЛИ ВМЕСТО 'CLICK' ПОСТАВИТЬ'SUBMIT' СТРАНИЦА ПЕРЕЗАГРУЖАЕТСЯ
+saveAddPopupBtn.addEventListener("click", createCard);
+
+
+// функция добавляет слушатель на элементы карточки
+function addListeners(el) {
+  el.querySelector(".element__basket").addEventListener("click",  deleteCard);
+  el.querySelector(".element__hart").addEventListener("click",  likeCard);
+  el.querySelector(".openPopupBtn__poster").addEventListener("click", () =>openImage(event));
+>>>>>>> Stashed changes
 }
 
 // функция удаляет карточку
